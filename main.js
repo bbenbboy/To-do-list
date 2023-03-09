cbb650219a57abc30e2526b43da4efa86010ee01
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-03-09 11:23:59 +0700

    added function for date stamp

--- main.js
+++ main.js
@@ -6,21 +6,38 @@ const textContainer = document.querySelector(".text-container");
 const submitBtn = document.querySelector("button");
 const resetButton = document.querySelector(".btn-reset");
 const alertMsg = document.querySelector(".alert");
-const checkbox = document.querySelector("#checkbox");
+
+const currentDate = new Date();
+const day = currentDate.getDate().toString().padStart(2, "0");
+const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+const year = currentDate.getFullYear();
+let hours = currentDate.getHours();
+const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+let amOrPm = "AM";
 
 const renderText = function (text) {
+  let hours = currentDate.getHours();
+  let amOrPm = "AM";
+  if (hours >= 12) {
+    amOrPm = "PM";
+    if (hours > 12) {
+      hours -= 12;
+    }
+  }
+  hours = hours.toString().padStart(2, "0");
   const html = `
     <div class="text-container">
         <hr />
-        <div class="container" >
+        <div class="output-container" >
             <h1>${text}</h1>
+            <p>${hours}:${minutes}${amOrPm} ${day}/${month}/${year}</p>
         </div>
     </div>
     `;
   boxContainer.insertAdjacentHTML("afterend", html);
 };
 
-submitBtn.addEventListener("click", function (e) {
+const handleSubmit = function (e) {
   e.preventDefault();
   if (textReceive.value === "") {
     textReceive.classList.add("active");
@@ -31,7 +48,10 @@ submitBtn.addEventListener("click", function (e) {
 
     renderText(textReceive.value);
   }
-});
+};
+
+submitBtn.addEventListener("click", handleSubmit);
+
 resetButton.addEventListener("click", function (e) {
   e.preventDefault();
   location.reload();
